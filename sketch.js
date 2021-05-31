8f288765d0b076d25a2c3ddfdfc7a52949750f1e
vibhudiwan <77778060+vibhudiwan@users.noreply.github.com>
2021-05-31 10:41:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth-300, windowHeight);
+  createCanvas(windowWidth-500, windowHeight);
   
   moon = createSprite(580, 350);
   moon.addImage(moonI);
